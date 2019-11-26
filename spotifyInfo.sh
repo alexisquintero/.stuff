360996917462866da65dd9fa9d91f879f1b8cb8e
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2019-11-26 15:32:26 -0300

    Add spotify integration to i3status

---
+++ spotifyInfo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+i3status --config ~/.config/i3/i3status | while :
+do
+  NAME=""
+  NAME=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 1 "xesam:title" | grep "variant.*" | grep -oP '.*"\K[^"]+')
+
+  ARTIST=""
+  ARTIST=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 "xesam:artist" | grep -v "xesam:artist" | grep -oP '.*"\K[^"]+')
+
+  read -r line
+  if [ "$NAME" ]
+  then
+    echo " $NAME - $ARTIST | $line" || exit 1
+  else
+    echo "$line" || exit 1
+  fi
+done
