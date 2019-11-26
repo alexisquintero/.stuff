f7fa6cf7bb72c557858297e80ce3a480bb14fb7b
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2019-11-26 15:35:15 -0300

    Small simplificatino to the spotify script

--- spotifyInfo.sh
+++ spotifyInfo.sh
@@ -2,10 +2,8 @@
 
 i3status --config ~/.config/i3/i3status | while :
 do
-  NAME=""
   NAME=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 1 "xesam:title" | grep "variant.*" | grep -oP '.*"\K[^"]+')
 
-  ARTIST=""
   ARTIST=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 "xesam:artist" | grep -v "xesam:artist" | grep -oP '.*"\K[^"]+')
 
   read -r line
