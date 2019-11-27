82088c50dd88afd04f506dcd6c03ec702aab75eb
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2019-11-27 10:05:09 -0300

    Only one call to dbus

--- spotifyInfo.sh
+++ spotifyInfo.sh
@@ -2,9 +2,9 @@
 
 if [ "$(pidof spotify)" ]
 then
-  NAME=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 1 "xesam:title" | grep "variant.*" | grep -oP '.*"\K[^"]+')
-
-  ARTIST=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 "xesam:artist" | grep -v "xesam:artist" | grep -oP '.*"\K[^"]+')
+  SPOTIFY_METADATA=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+  NAME=$(echo "$SPOTIFY_METADATA" | grep -A 1 "xesam:title" | grep "variant.*" | grep -oP '.*"\K[^"]+')
+  ARTIST=$(echo "$SPOTIFY_METADATA" | grep -A 2 "xesam:artist" | grep -v "xesam:artist" | grep -oP '.*"\K[^"]+')
 
   echo " $NAME - $ARTIST | "
 else
