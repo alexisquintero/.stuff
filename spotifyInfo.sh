f281bab5782b26624e6b4fbf4582613fc43e1899
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-02-27 13:11:58 -0300

    Replace sed with tr

--- spotifyInfo.sh
+++ spotifyInfo.sh
@@ -9,7 +9,7 @@ then
     --method org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player "Metadata")
 
   NAME=$(echo "$SPOTIFY_METADATA" | grep -oP "(?<='xesam:title': <['\"]).*(?=['\"]>,)")
-  ARTIST=$(echo "$SPOTIFY_METADATA" | grep -oP "(?<='xesam:artist': <\[['\"]).*(?=['\"]\]>,)" | sed s/[][\'\"]//g)
+  ARTIST=$(echo "$SPOTIFY_METADATA" | grep -oP "(?<='xesam:artist': <\[['\"]).*(?=['\"]\]>,)" | tr -d "]['\"")
 
   echo "♫ $NAME - $ARTIST | "
 else
